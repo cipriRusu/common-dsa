a6593290bfa6ff72760a457a23ca60cdac1f4bdc
Cipri Rusu <cipri91rusu@gmail.com>
2026-07-15 15:12:15 +0200

    Initial Queue implementation

---
+++ Data Structures/Queue.js
@@ -0,0 +1,27 @@
+class Queue {
+    constructor() {
+        this.dataSource = [];
+        this.Length=0;
+        this.Enqueue;
+        this.Dequeue;
+        this.Front;
+        this.Tail;
+    }
+
+    Enqueue(element) {
+        this.dataSource[this.Length++] = element;
+    }
+
+    Dequeue() {
+        this.Length--;
+        return this.dataSource.shift();
+    }
+
+    Front() {
+        return this.dataSource[0];
+    }
+
+    Tail() {
+        return this.dataSource[this.Length-1];
+    }
+}
